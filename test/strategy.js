4d48ba70a1fcd499ed842dffb95c42bd3557ed90
Giorgio Aresu <giorgioaresu@gmail.com>
2016-10-05 21:25:02 +0200

    ciao

--- test/strategy.js
+++ test/strategy.js
@@ -35,32 +35,6 @@ beforeEach(() => {
     'callAmount': 50,
     'minimumRaiseAmount': 100
   };
-  coppiaAlta = {
-    'commonCards': [],
-    'players': [
-      {
-        'id': 0,
-        'name': 'Arale',
-        'status': 'active',
-        'cards': [
-          {
-            'rank': 'K',
-            'type': 'C'
-          },
-          {
-            'rank': 'K',
-            'type': 'H'
-          }
-        ],
-        'chips': 500,
-        'chipsBet': 0
-      }
-    ],
-    'db': 2,
-    'me': 0,
-    'callAmount': 50,
-    'minimumRaiseAmount': 100
-  };
 });
 
 const gamestateFactory = (cards, chips, call, raise) => ({
@@ -95,15 +69,23 @@ describe('- Strategy -', () => {
     ], 500, 50, 100);
     const bet = player.bet(gamestate);
 
-    expect(bet).to.be.above(gamestate.callAmount);
+    expect(bet).to.be.above(50);
   });
 
   it('should go all in if high pair', () => {
-    const gamestate = coppiaAlta;
+    const gamestate = gamestateFactory([
+      {
+        'rank': 'K',
+        'type': 'C'
+      },
+      {
+        'rank': 'K',
+        'type': 'H'
+      }
+    ], 500, 50, 100);
     const bet = player.bet(gamestate);
-    const allIn = gamestate.players[gamestate.me].chips;
 
-    expect(bet).to.equal(allIn);
+    expect(bet).to.equal(500);
   });
 
   it('should fold otherwise', () => {
