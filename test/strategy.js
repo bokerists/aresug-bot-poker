4fdb83c8d45afc74e7f7230e206cbc4be67053c4
Giorgio Aresu <giorgioaresu@gmail.com>
2016-10-05 20:42:03 +0200

    ciao

--- test/strategy.js
+++ test/strategy.js
@@ -7,37 +7,37 @@ chai.use(sinonChai);
 const player = require('../player');
 
 beforeEach(() => {
-
+  const gs1 = {
+    'commonCards': [],
+    'players': [
+      {
+        'id': 0,
+        'name': 'Arale',
+        'status': 'active',
+        'cards': [
+          {
+            'rank': 'K',
+            'type': 'C'
+          },
+          {
+            'rank': 'A',
+            'type': 'H'
+          }
+        ],
+        'chips': 500,
+        'chipsBet': 0
+      }
+    ],
+    'db': 2,
+    'me': 0,
+    'callAmount': 50,
+    'minimumRaiseAmount': 100
+  };
 });
 
 describe('- Strategy -', () => {
   it('should go all in if 2 figures or aces', () => {
-    const gamestate = {
-      'commonCards': [],
-      'players': [
-        {
-          'id': 0,
-          'name': 'Arale',
-          'status': 'active',
-          'cards': [
-            {
-              'rank': 'K',
-              'type': 'C'
-            },
-            {
-              'rank': 'A',
-              'type': 'H'
-            }
-          ],
-          'chips': 500,
-          'chipsBet': 0
-        }
-      ],
-      'db': 2,
-      'me': 0,
-      'callAmount': 50,
-      'minimumRaiseAmount': 100
-    };
+    const gamestate = gs1;
     const bet = player.bet(gamestate);
     const allIn = gamestate.players[gamestate.me].chips;
     expect(bet).to.eql(allIn);
